4a3a0be29b7666a87f38bf7b90190b497a2bb401
RoddaSreeCharanReddy <127850836+RoddaSreeCharanReddy@users.noreply.github.com>
2025-03-24 00:12:21 +0530

    Create GCP_code.sh

---
+++ GCP_code.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Set variables
+REGION="asia-south1"
+INSTANCE_TEMPLATE="monitoring-template"
+MIG_NAME="monitoring-mig"
+
+# Create instance template
+gcloud compute instance-templates create $INSTANCE_TEMPLATE \
+  --machine-type=e2-medium \
+  --image-family=debian-11 \
+  --image-project=debian-cloud \
+  --boot-disk-size=20GB \
+  --tags=http-server \
+  --metadata=startup-script='#! /bin/bash
+    sudo apt update
+    sudo apt install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    docker run -d --name prometheus --network host prom/prometheus
+    docker run -d --name grafana --network host grafana/grafana'
+
+# Create Managed Instance Group (MIG)
+gcloud compute instance-groups managed create $MIG_NAME \
+  --base-instance-name monitoring-instance \
+  --template $INSTANCE_TEMPLATE \
+  --size 1 \
+  --region $REGION \
+  --update-policy-type=proactive
+
+# Enable auto-scaling
+gcloud compute instance-groups managed set-autoscaling $MIG_NAME \
+  --max-num-replicas 5 \
+  --target-cpu-utilization 0.75 \
+  --region $REGION \
+  --cool-down-period 90
+
+echo "GCP Auto-scaling setup complete!"
